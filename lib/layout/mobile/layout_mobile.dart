ce72ba8aebcd7d08d103d3633e87c9e165853623
Pedro Siqueira <pedro.siqueira1999@hotmail.com>
2024-07-04 22:50:14 -0300

    layout mobile content

--- lib/layout/mobile/layout_mobile.dart
+++ lib/layout/mobile/layout_mobile.dart
@@ -9,8 +9,28 @@ class LayoutMobile extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       drawer: const CustomDrawer(),
-      body: const Center(
-        child: Text('Mobile Layout'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+            child: CustomScrollView(
+          slivers: [
+            SliverGrid.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.red,
+                  height: 100,
+                  width: 100,
+                );
+              },
+            )
+          ],
+        )),
       ),
     );
   }
